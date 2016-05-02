97cdaa8046b67a8145bdafd9a4158afaf8176d88
katebolger <kate_bolger@greenwich.k12.ct.us>
2016-05-01 21:37:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " W3Schools. All rights reserved.</p>";
+"<p>&copy;  " + new Date().getFullYear() + " Joe Schiro. Thanks for reading.</p>";
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>Home</a></li>" +
